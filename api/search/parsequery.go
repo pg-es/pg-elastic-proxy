aa24fa1b5573b43449ab1ea38ae0ea9d540c654e
Hosh Sadiq <600045+hoshsadiq@users.noreply.github.com>
2026-08-22 03:50:02 +0400

    fix: replace self-assignment operator hacks with idiomatic _ = operator

--- api/search/parsequery.go
+++ api/search/parsequery.go
@@ -48,7 +48,7 @@ func parseMatchPhraseQuery(rawQuery map[string]interface{}, query *db.Query, map
 					operator = vv.(string)
 				}
 			}
-			operator = operator // Silent not used variable
+			_ = operator
 			fieldMapping, ok := utils.GetFieldMapping(mapping, fieldName)
 			if ok && len(fieldMapping.Analyzer) > 0 {
 				whereClause = fmt.Sprintf("to_tsvector('%s', document->'%s') @@ phraseto_tsquery('%s', '%s')", fieldMapping.Analyzer, fieldName, fieldMapping.Analyzer, queryString)
@@ -83,7 +83,7 @@ func parseMatchQuery(rawQuery map[string]interface{}, query *db.Query, mapping m
 					operator = vv.(string)
 				}
 			}
-			operator = operator // Silent not used variable
+			_ = operator
 			fieldMapping, ok := utils.GetFieldMapping(mapping, fieldName)
 			if ok && len(fieldMapping.Analyzer) > 0 {
 				whereClause = fmt.Sprintf("to_tsvector('%s', document->'%s') @@ to_tsquery('%s', '%s')", fieldMapping.Analyzer, fieldName, fieldMapping.Analyzer, queryString)
